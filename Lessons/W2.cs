6467fd63de58a23640bbe8e9e093a2274bf0da11
dthom7473 <dallinwthomas@gmail.com>
2024-04-29 12:26:31 -0600

    Added Lessons File

---
+++ Lessons/W2.cs
@@ -0,0 +1,24 @@
+int x = 0; //you should normally give variables a starting value
+
+if (x==0){
+    //do some fun stuff here
+    Console.WriteLine("TEXT");
+}
+
+while (x < 3) {
+    Console.WriteLine("TEXT");
+    x = x + 1; //can also use x += 1
+}
+
+do {
+    Console.WriteLine("TEXT");
+
+} while (x < 3); //this gaurentees that the code will run at least once
+
+foreach (x in something) {
+    //insert fun code
+}
+
+for (; ; ) { //(initializer; true/false test; modifier)
+
+}
