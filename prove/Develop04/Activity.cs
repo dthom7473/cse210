83b13f002d39256b256936d8923f6b6b9b4a0bec
dthom7473 <dallinwthomas@gmail.com>
2024-06-10 12:10:49 -0600

    Begun develop04

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,16 @@
+class Activity 
+{
+    //attributes
+    protected int _duration;
+    protected string _activityName;
+    protected string _activityDescription;
+
+    public Activity(int durParam, string nameParam, string descriptionParam)
+    {
+        _duration = durParam;
+        _activityName = nameParam;
+        _activityDescription = descriptionParam;
+
+    }
+
+}
---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,9 @@
+class BreathingActivity : Activity
+{
+    public BreathingActivity(int durParam, string nameParam, string descriptionParam)
+        : base(durParam, nameParam, descriptionParam) //just refer to parent
+    {
+        //refering to paraent param 
+    }
+
+}
